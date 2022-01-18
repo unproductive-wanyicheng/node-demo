b743c5644ec148053a348fa28681128446d15ef7
万宜成 <17775728499@163.com>
2022-01-18 15:24:00 +0800

    Add files via upload

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,9 @@
 module.exports = {
     'db': {
-        host : 'exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        port : 3306,//端口号
-        database : 'cb41o78h9pjjllwq',//数据库名
-        user : 'tg91n46ncozj2abc',//数据库用户名
-        password : 'eq3viv9ph78dmbua'//数据库密码
+        host : '47.102.127.65',
+        port : 5433,//端口号
+        database : 'db7fdac16899654c309a3760f9f003f998wechatdemo',//数据库名
+        user : 'Wyc520Ypt',//数据库用户名
+        password : 'Wyc520Ypt1314'//数据库密码
     }
 };
