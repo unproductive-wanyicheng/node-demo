0fc3b2237aa4c46a3c8a0378f661326e54202cb4
万宜成 <17775728499@163.com>
2022-01-22 14:53:06 +0800

    Add files via upload

--- server-code/routes/api.js
+++ server-code/routes/api.js
@@ -4,11 +4,19 @@ var db = require('../dao/query.js');
 
 /* GET api listing. */
 router.get('/users', function (req, res, next) {
-	db.query('select * from t_user', [], function (result, fields) {
+	db.query('select * from t_user', [], function (err, result, fields) {
+		if (err) {
+			res.send({
+				status: 'failed',
+				reason: `${err}`
+			})
+			return;
+		}
 		console.log('查询结果：');
 		console.log(result);
 		res.send(result);
 	});
+	
 });
 
 router.post('/register', function (req, res, next) {
@@ -19,38 +27,44 @@ router.post('/register', function (req, res, next) {
 			reason: '用户名 密码 不得为空'
 		})
 	}
-	try {
-		// 先查这个用户存在与否
-		db.query('select username from t_user as u where u.username = ?', [username], function (result, fields) {
-			if (result && result.length) {
+	// 先查这个用户存在与否
+	db.query('select username from t_user as u where u.username = ?', [username], function (err, result, fields) {
+		if (err) {
+			res.send({
+				status: 'failed',
+				reason: `${err}`
+			})
+			return;
+		}
+		if (result && result.length) {
+			res.send({
+				status: 'failed',
+				reason: `用户名 ${username} 已存在`
+			})
+			return;
+		}
+		// 插入这条新数据
+		db.query('insert into t_user (username, password) values (?, ?)', [username, password], function (err, uResult, fields) {
+			if (err) {
 				res.send({
 					status: 'failed',
-					reason: `用户名 ${username} 已存在`
+					reason: `${err}`
 				})
 				return;
 			}
-			// 插入这条新数据
-			db.query('insert into t_user (username, password) values (?, ?)', [username, password], function (uResult, fields) {
-				if (uResult) {
-					res.send({
-						status: 'ok',
-						data: {
-							username: username,
-							id: uResult.insertId,
-							token: `user ${username} logined`
-						}
-					})
-				}
-				
-			});
+			if (uResult) {
+				res.send({
+					status: 'ok',
+					data: {
+						username: username,
+						id: uResult.insertId,
+						token: `user ${username} logined`
+					}
+				})
+			}
+			
 		});
-
-	} catch (e) {
-		res.send({
-			status: 'failed',
-			reason: `${e}`
-		})
-	}
+	});
 	
 });
 
