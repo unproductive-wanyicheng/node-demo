320c388808caff6959f6cb9043378263931719d1
万宜成 <17775728499@163.com>
2022-01-22 14:16:15 +0800

    Delete prod-db4free.js

--- config/prod-db4free.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'db': {
-        host : 'db4free.net',
-        port : 3306,//端口号
-        database : 'wechatdemo',//数据库名
-        user : 'wanyicheng',//数据库用户名
-        password : 'wanyicheng'//数据库密码
-    }
-};
