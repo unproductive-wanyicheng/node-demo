94db13ff26e6c0589f514e0dd15a861a5bd1fa7f
万宜成 <17775728499@163.com>
2022-01-22 12:53:00 +0800

    Add files via upload

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,16 @@
 module.exports = {
+    // 'db': {
+    //     host : '47.102.127.65',
+    //     port : 5433,//端口号
+    //     database : 'db7fdac16899654c309a3760f9f003f998wechatdemo',//数据库名
+    //     user : 'wanyicheng',//数据库用户名
+    //     password : 'WanYiCheng123'//数据库密码
+    // },
     'db': {
-        host : '47.102.127.65',
-        port : 5433,//端口号
-        database : 'db7fdac16899654c309a3760f9f003f998wechatdemo',//数据库名
-        user : 'Wyc520Ypt',//数据库用户名
-        password : 'Wyc520Ypt1314'//数据库密码
+        host : 'db4free.net',
+        port : 3306,//端口号
+        database : 'wechatdemo',//数据库名
+        user : 'wanyicheng',//数据库用户名
+        password : 'wanyicheng'//数据库密码
     }
 };
