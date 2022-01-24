683e02f04cbad6a44430bbbe6e1933f45a77de13
万宜成 <17775728499@163.com>
2022-01-24 20:41:59 +0800

    Add files via upload

--- server-code/routes/api.js
+++ server-code/routes/api.js
@@ -1,6 +1,49 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../dao/query.js');
+let jwt = require('jwt-simple');
+let secret = 'wanyicheng520yupengtao';
+
+router.use(function (req, res, next) {
+	console.log('%s %s %s', req.method, req.url, req.path);
+	// 对于 api 路径下 需要鉴权 除了 登录 和 注册 之外的接口
+	let reg1 = /\/register/gi;
+	let reg2 = /\/login/gi;
+	if (reg1.test(req.url) || reg2.test(req.url)) {
+		next();
+		return;
+	}
+	let Authorization = req.headers.authorization || '';
+    let [,token] = Authorization.split(' ');
+    if(token){
+        try{
+            let r = jwt.decode(token,secret);
+			console.log(r)
+            if (r.expiresIn < (new Date()).getTime()) {
+				res.send({
+					code: 3,
+					status: 'failed',
+					reason: `用户未登录`
+				})
+			} else {
+				next();
+			}
+        }catch(e){
+			res.send({
+				code: 3,
+				status: 'failed',
+				reason: `用户未登录`
+			})
+        }
+    }else{
+        res.send({
+			code: 3,
+			status: 'failed',
+			reason: `用户未登录`
+		})
+    }
+
+  })
 
 /* GET api listing. */
 router.get('/users', function (req, res, next) {
@@ -24,6 +67,7 @@ router.post('/register', function (req, res, next) {
 	if (!username || !password) {
 		res.send({
 			status: 'failed',
+			code: 2,
 			reason: '用户名 密码 不得为空'
 		})
 	}
@@ -39,6 +83,7 @@ router.post('/register', function (req, res, next) {
 		if (result && result.length) {
 			res.send({
 				status: 'failed',
+				code: 1,
 				reason: `用户名 ${username} 已存在`
 			})
 			return;
@@ -53,17 +98,67 @@ router.post('/register', function (req, res, next) {
 				return;
 			}
 			if (uResult) {
+				let tokenData = {
+					username,
+					expiresIn: (new Date()).getTime() + 2 * 24 * 60 * 60 * 1000
+				}
+				let token =  jwt.encode(tokenData, secret);
 				res.send({
 					status: 'ok',
+					code: 0,
 					data: {
 						username: username,
 						id: uResult.insertId,
-						token: `user ${username} logined`
+						token: token
 					}
 				})
 			}
 			
 		});
+	});
+	
+});
+
+router.post('/login', function (req, res, next) {
+	let {username, password} = req.body;
+	if (!username || !password) {
+		res.send({
+			status: 'failed',
+			code: 2,
+			reason: '用户名 密码 不得为空'
+		})
+	}
+	// 先查这个用户存在与否
+	db.query('select username from t_user as u where u.username = ?', [username], function (err, result, fields) {
+		if (err) {
+			res.send({
+				status: 'failed',
+				reason: `${err}`
+			})
+			return;
+		}
+		if (result && result.length) {
+			let tokenData = {
+				username,
+				expiresIn: (new Date()).getTime() + 2 * 24 * 60 * 60 * 1000
+			}
+			let token =  jwt.encode(tokenData, secret);
+			res.send({
+				status: 'ok',
+				code: 0,
+				data: {
+					username: result[0].username,
+					id:  result[0].id,
+					token: token
+				}
+			})
+		} else {
+			res.send({
+				status: 'failed',
+				code: 4,
+				reason: `用户名 ${username} 不存在`
+			})
+		}
 	});
 	
 });
