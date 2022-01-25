b5a5db61010a5fa2aad0822824042f20992841cf
万宜成 <17775728499@163.com>
2022-01-25 19:47:59 +0800

    Add files via upload

--- frontend-code/main.js
+++ frontend-code/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue';
 import App from './src/App.vue';
-  
+import VueRouter from 'vue-router';  
 // Vue.createApp(App).mount('#app');
+import { routes } from './src/router/index'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes // (缩写) 相当于 routes: routes
+})
 
 var app = new Vue({
     el: '#app',
+    router,
     render: (h) => h(App) 
   })
