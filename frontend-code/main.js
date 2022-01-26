1e81a1911210a6297942f55e2469cddc2d6965de
万宜成 <17775728499@163.com>
2022-01-26 13:07:32 +0800

    Add files via upload

--- frontend-code/main.js
+++ frontend-code/main.js
@@ -3,15 +3,22 @@ import App from './src/App.vue';
 import VueRouter from 'vue-router';  
 // Vue.createApp(App).mount('#app');
 import { routes } from './src/router/index'
+import Vuex from 'vuex'
+import { stores } from './src/store/index'
 
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 const router = new VueRouter({
+  mode: 'history',
   routes // (缩写) 相当于 routes: routes
 })
+
+const store = new Vuex.Store(stores);
 
 var app = new Vue({
     el: '#app',
     router,
+    store,
     render: (h) => h(App) 
   })
