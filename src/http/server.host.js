2af932937a4f2d93b90f293c8f49d24719d11608
万宜成 <17775728499@163.com>
2022-01-26 13:04:09 +0800

    Delete src directory

--- src/http/server.host.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    host: process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'https://wan-node-demo.herokuapp.com'
-}
--- src/router/index.js
+++
@@ -1,9 +0,0 @@
-const Chat = () => import('../components/chat.vue');
-const Home = () => import('../components/home.vue');
-
-module.exports = {
-    routes: [
-        { path: '/', component: Home },
-        { path: '/chat', component: Chat },
-    ]
-}
